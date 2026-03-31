bc00d1e6df8ef3acd2fef8e18184c1f65a873ee5
Yuhur <yuhur1985@gmail.com>
2026-03-31 19:16:56 +0300

    Restore balanced detection cadence

--- src/main.cpp
+++ src/main.cpp
@@ -31,7 +31,7 @@
 std::atomic<bool> g_running{true};
 
 namespace {
-constexpr int kDetectionIntervalFrames = 1;
+constexpr int kDetectionIntervalFrames = 2;
 
 struct CameraPipelineState {
     ByteTracker tracker;
